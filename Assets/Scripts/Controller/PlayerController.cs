171369875a29074a34c63848db25c5f66106fba6
“Jeongwonseok” <jws4281@nate.com>
2019-07-29 21:43:04 +0900

    Crosshair-2
    크로스헤어 & 뷰 회전

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -7,13 +7,40 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] Transform tf_Crosshair;
 
+    [SerializeField] Transform tf_Cam;
+
+    [SerializeField] float sightSensivitity; // 고개의 움직임 속도
+    [SerializeField] float lookLimitX;
+    [SerializeField] float lookLimitY;
+    float currentAngleX;
+    float currentAngleY;
 
 
     // Update is called once per frame
     void Update()
     {
         CrosshairMoving();
+        ViewMoving();
         
+    }
+
+    void ViewMoving()
+    {
+        if (tf_Crosshair.localPosition.x > (Screen.width / 2 - 100) || tf_Crosshair.localPosition.x < (-Screen.width / 2 + 100))
+        {
+            currentAngleY += (tf_Crosshair.localPosition.x > 0) ? sightSensivitity : -sightSensivitity;
+            currentAngleY = Mathf.Clamp(currentAngleY, -lookLimitX, lookLimitX);
+            
+        }
+
+        if (tf_Crosshair.localPosition.y > (Screen.height / 2 - 100) || tf_Crosshair.localPosition.y < (-Screen.height / 2 + 100))
+        {
+            currentAngleX += (tf_Crosshair.localPosition.y > 0) ? -sightSensivitity : sightSensivitity;
+            currentAngleX = Mathf.Clamp(currentAngleX, -lookLimitY, lookLimitY);
+
+        }
+
+        tf_Cam.localEulerAngles = new Vector3(currentAngleX, currentAngleY, tf_Cam.localEulerAngles.z);
     }
 
     void CrosshairMoving()
