f5f5dc9b8ee8ed77334b108b408922d04d5c3588
“Jeongwonseok” <jws4281@nate.com>
2019-07-30 19:39:44 +0900

    Interaction-1
    봉란 캐릭터 커서 접촉 시 크로스헤어 변경

---
+++ Assets/Scripts/Controller/InteracrionController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteracrionController : MonoBehaviour
+{
+    [SerializeField] Camera cam;
+
+    RaycastHit hitInfo;
+
+    [SerializeField] GameObject go_NormalCrosshair;
+    [SerializeField] GameObject go_InteractiveCrosshair;
+
+    bool isContact = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckObject();
+    }
+
+    void CheckObject()
+    {
+        Vector3 t_MousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+
+        if(Physics.Raycast(cam.ScreenPointToRay(t_MousePos), out hitInfo, 100))
+        {
+            Contact();
+        }
+        else
+        {
+            NotContact();
+        }
+    }
+
+    void Contact()
+    {
+        if(hitInfo.transform.CompareTag("Interaction"))
+        {
+            if(!isContact)
+            {
+                isContact = true;
+                go_InteractiveCrosshair.SetActive(true);
+                go_NormalCrosshair.SetActive(false);
+            }
+
+        }
+        else
+        {
+            NotContact();
+        }
+    }
+
+    void NotContact()
+    {
+        if(isContact)
+        {
+            isContact = false;
+            go_InteractiveCrosshair.SetActive(false);
+            go_NormalCrosshair.SetActive(true);
+        }
+    }
+}
